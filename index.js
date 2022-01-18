ee0204e93e5ef2f1111ad09d23c14a82a36f106e
geekhunger <jack0088@me.com>
2022-01-18 15:44:46 +0000

    added manadatory requirements, fixed some typos, extended mkfile to accept "w" or "a" flags for example

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 const fn = module.exports = {}
 
+const {
+    join,
+    dirname,
+    basename,
+    extname
+} = require("path")
+
+const {
+    statSync,
+    existsSync,
+    readdirSync,
+    mkdirSync,
+    readFileSync,
+    writeFileSync,
+    rmSync
+} = require("fs")
+
+const mimetype = path => {
+    return require("mime").types[extname(path).substr(1)]
+}
+
+const unit_lookup = (value = 0) => {
+    return {
+        value: value,
+        set byte(n)     {this.value = n},
+        set kilobyte(n) {this.value = n * 1024},
+        set megabyte(n) {this.value = n * 1024 * 1024},
+        set gigabyte(n) {this.value = n * 1024 * 1024 * 1024},
+        get byte()      {return this.value},
+        get kilobyte()  {return this.value / 1024},
+        get megabyte()  {return this.value / 1024 / 1024},
+        get gigabyte()  {return this.value / 1024 / 1024 / 1024}
+    }
+}
+
 
 fn.mkfolder = path => { // create directory (recursevly)
     /*fn.assert(
@@ -7,15 +42,15 @@ fn.mkfolder = path => { // create directory (recursevly)
         `Folder names are not allowed to have extensions of type file! In other words, '${basename(path, extname(path))}${extname(path)}' could not be created inside of '${dirname(path)}' because it's not allowed to have '${extname(path)}'.`
     )*/
     if(/^\./i.test(path)) path += "/./" // convert dot-files into folders!
-    path = resolve(ROOTPATH.toString(), path) // restrict folder creation to project directory!
-    if(!fs.existsSync(path)) return fs.mkdirSync(path, {recursive: true})
+    //path = resolve(ROOTPATH.toString(), path) // restrict folder creation to project directory!
+    if(!existsSync(path)) return mkdirSync(path, {recursive: true})
 }
 
 
-fn.mkfile = (path, content, permissions = 0o744) => { // create file
+fn.mkfile = (path, content, action = "w", mode = 0o744) => { // create file
     fn.mkfolder(dirname(path))
-    try { // try-catch needed because fs.writeFileSync does not have a return value, instead it throws an error on failure
-        fs.writeFileSync(path, content, {mode: permissions})
+    try { // try-catch needed because writeFileSync does not have a return value, instead it throws an error on failure
+        writeFileSync(path, content, {flag: action, mode: mode})
         return true
     } catch(failure) {
         console.error(`Could not create file '${path}' because of error: ${failure.message}`)
@@ -26,10 +61,10 @@ fn.mkfile = (path, content, permissions = 0o744) => { // create file
 
 fn.rmfile = fn.rmfolder = path => { // remove file or folder recursevly
     try {
-        fs.rmSync(path, {recursive: true, force: true})
+        rmSync(path, {recursive: true, force: true})
         return true
     } catch(failure) {
-        console.error(`Could not create file '${path}' because of error: ${failure.message}`)
+        console.error(`Could not remove file '${path}' because of error: ${failure.message}`)
         return false
     }
 }
@@ -38,17 +73,17 @@ fn.rmfile = fn.rmfolder = path => { // remove file or folder recursevly
 fn.catfile = fn.catfolder = (path, encoding = "base64") => { // read files recursevly (@path can be a single filename, an array of many filenames, a single folder name, an array of many folder names or even a mixed array of file and folder names)
     const files = []
     for(const file of !Array.isArray(path) ? [path] : path) {
-        const asset = fs.statSync(file)
+        const asset = statSync(file)
         if(asset.isFile()) {
             files.push({
-                content: fs.readFileSync(file, {encoding: encoding}),
+                content: readFileSync(file, {encoding: encoding}),
                 encoding: encoding,
                 mime: mimetype(file),
-                size: fn.storage_unit_lookup(asset.size),
+                size: unit_lookup(asset.size),
                 name: basename(file),
             })
         } else if(asset.isDirectory()) {
-            const paths = fs.readdirSync(file).map(name => join(file, name))
+            const paths = readdirSync(file).map(name => join(file, name))
             files.push(fn.catfile(paths, encoding))
         }
     }
